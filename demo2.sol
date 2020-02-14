a23599f83da6e263d177a31f6c7116d5a36bcc37
iavl <z6521256@gmail.com>
2020-02-14 21:51:56 +0800

    Update demo2.sol

--- demo2.sol
+++ demo2.sol
@@ -4,7 +4,7 @@ contract MsgRecall {
     // 事件
     // 每次成功的上链记录，都会创建一个Recall事件
     // 可以通过txHash查询对应的事件
-    event Recall(address from, address to, uint timestamp, bytes32 pk);
+    event Recall(address from, address to, uint timestamp, int64 pk);
 
 
     // 用户授权的结构体, 表示from发起一次授权请求，删除和to的聊天记录
@@ -15,7 +15,7 @@ contract MsgRecall {
     // @timestamp 表示记录时间
 
     struct st {
-        bytes32 pubkey;
+        int64 pubkey;
         uint timestamp;
     }
 
@@ -23,15 +23,15 @@ contract MsgRecall {
     mapping (address => mapping (address => st)) Mapping;
 
     // 消息撤回记录上链接口，r,s,v表示用户授权签名
-    function recall(address from, address to, uint timestamp, bytes32 pk, bytes32 s, byte v) public {
+    function recall(address from, address to, uint timestamp, int64 pk, int64 r, int64 s) public {
         // 验证from和pubkey是否一致
 
         // 验证用户授权的签名
-        // address addr = ecrecoverDecode(from, to, timestamp, r, s, v);
+        // address addr = ecrecoverDecode(from, to, timestamp, r, s);
         //if (addr != from) revert(); // 如果验签不通过，则回滚
 
         st memory sp;
-        // sp.pubkey = pk;
+        sp.pubkey = pk;
         sp.timestamp = timestamp;
         // sp.t = t;
         // 记录from, to和timestamp
@@ -42,17 +42,17 @@ contract MsgRecall {
     }
 
     // 使用ecrecover恢复出公钥
-    function ecrecoverDecode(address from, address to, uint timestamp, bytes32 r, bytes32 s, byte v) public  returns (address addr) {
+    function ecrecoverDecode(address from, address to, uint timestamp, int64 r, int64 s) public  returns (address addr) {
         // 将from, to, timestamp拼接起来，做sha256，然后验签
         // sha256(bytes memory) returns (bytes32)
         // bytes32 hash = sha256(from, to, timestamp);
         // 从椭圆曲线签名中恢复与公钥相关的地址，或在出错时返回零。函数参数对应于签名的ECDSA值: r – 签名的前32字节; s: 签名的第二个32字节; v: 签名的最后一个字节
-        // ecrecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) returns (address)
-        // addr = ecrecover(hash, v, r, s);
+        // ecrecover(bytes32 hash, bytes32 r, bytes32 s) returns (address)
+        // addr = ecrecover(hash, r, s);
     }
 
     // 查询接口
-    function queryParams(address to, address from) public  returns (bytes32 pubkey, uint timestamp) {
+    function queryParams(address to, address from) public  returns (int64 pubkey, uint timestamp) {
         st memory s = Mapping[to][from];
         pubkey = s.pubkey;
         timestamp = s.timestamp;
